a12384a5c3856e9c03d75459a1e867312f21689a
DesarrolloCEnergy <jose.romero@canelo.energy>
2024-12-26 16:07:16 -0600

    conexión 1

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<?php phpinfo() ?>
+<?php
+
+require_once 'connec.php';
+require_once 'entityl.php';
+require_once 'model.php';
+require_once 'entity.php';
+$connecc = new Connecc();
+$entity = new Sesion();
+$model = new Model();
+
+// foreach ((array)$connecc->ConSe() as $r) :
+//     echo $r->__GET('nameUser');
+//     echo $r->__GET('emailUser');
+//     echo $r->__GET('phoneUser');
+//     echo $r->__GET('nameCompany');
+//     echo $r->__GET('nameProfile');
+// endforeach;
+
+if($result = $connecc->ConSe() != null){
+        echo "te conectaste";
+    }else{
+        echo "no se pudo";
+    }
+
+?>
