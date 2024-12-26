36d665e1b7a2cdfb53e565321b273ceacf9a30b9
DesarrolloCEnergy <jose.romero@canelo.energy>
2024-12-26 16:51:18 -0600

    conexión 1

--- index.php
+++ index.php
@@ -8,13 +8,32 @@ $connecc = new Connecc();
 $entity = new Sesion();
 $model = new Model();
 
-foreach ((array)$connecc->ConSe() as $r) :
-    echo $r->__GET('nameUser');
-    echo $r->__GET('emailUser');
-    echo $r->__GET('phoneUser');
-    echo $r->__GET('nameCompany');
-    echo $r->__GET('nameProfile');
-endforeach;
+?>
+
+<table class="table table-hover table-md">
+    <thead>
+        <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Empresa</th>
+            <th scope="col">Perfil</th>
+
+        </tr>
+    </thead>
+        <?php foreach ((array)$model->UserList() as $r) : ?>
+    <tr>
+        <td><?php echo $r->__GET('nameUser'); ?></td>
+        <td><?php echo $r->__GET('emailUser'); ?></td>
+        <td><?php echo $r->__GET('phoneUser'); ?></td>
+        <td><?php echo $r->__GET('nameCompany'); ?></td>
+        <td><?php echo $r->__GET('nameProfile'); ?></td>
+
+    </tr>
+    <?php endforeach; ?>
+</table>
+
+<?php
 
 // if($result = $connecc->ConSe() != null){
 //         echo "te conectaste";
