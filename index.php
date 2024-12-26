7af1549929af0e57dbe6017741c3766134a21d03
DesarrolloCEnergy <jose.romero@canelo.energy>
2024-12-26 16:10:08 -0600

    conexión 1

--- index.php
+++ index.php
@@ -8,13 +8,13 @@ $connecc = new Connecc();
 $entity = new Sesion();
 $model = new Model();
 
-// foreach ((array)$connecc->ConSe() as $r) :
-//     echo $r->__GET('nameUser');
-//     echo $r->__GET('emailUser');
-//     echo $r->__GET('phoneUser');
-//     echo $r->__GET('nameCompany');
-//     echo $r->__GET('nameProfile');
-// endforeach;
+foreach ((array)$connecc->ConSe() as $r) :
+    echo $r->__GET('nameUser');
+    echo $r->__GET('emailUser');
+    echo $r->__GET('phoneUser');
+    echo $r->__GET('nameCompany');
+    echo $r->__GET('nameProfile');
+endforeach;
 
 if($result = $connecc->ConSe() != null){
         echo "te conectaste";
