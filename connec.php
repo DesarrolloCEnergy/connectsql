73bc999c2468be6daf606c3452de0fe01dc56d5d
DesarrolloCEnergy <jose.romero@canelo.energy>
2024-12-26 16:20:47 -0600

    conexión 1

--- connec.php
+++ connec.php
@@ -13,14 +13,14 @@ class Connecc
 				$driver = '{ODBC Driver 17 for SQL Server}';
                 $hosport = $hostname.":".$port;
                 //require("varl.php");
-                $this->pdo = new PDO("sqlsrv:server=".$hostname.";Database=".$dbname.",". $username.",".$password);
+                $this->pdo = new PDO("sqlsrv:server = tcp:".$hosport.";Database=".$dbname.",". $username.",".$password);
                 // $this->pdo = new PDO("sqlsrv:server = tcp:transbus-server.database.windows.net; Database = dbtg", "admindbce", "C4n3l0BD!!.");
                 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
-            catch(Exception $e)
+            catch(PDOException $e)
             {
 				print("Error connecting to SQL Server.");
-                die($e->getMessage());
+                die(print_r($e));
             }
         }
 
