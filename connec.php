a7b9ffc86b34a26e95d19ed4735cbd15dd520c55
DesarrolloCEnergy <jose.romero@canelo.energy>
2024-12-26 17:48:09 -0600

    conexión 1

--- connec.php
+++ connec.php
@@ -5,19 +5,13 @@ class Connecc
     private $pdo;
         public function __construct(){
             try{
-                $hostname = 'tcp:transbus-server.database.windows.net,1433';
-                $dbname = 'tbg';
-                $username = 'transbus-server-admin';
-                $password = "Tr4nsb4s!!.";
-                $port = 1433;
-				$driver = '{ODBC Driver 17 for SQL Server}';
-                $hosport = $hostname.":".$port;
-                //require("varl.php");
-                ////$this->pdo = new PDO("sqlsrv:server = tcp:".$hosport.";Database=".$dbname.",". $username.",".$password);
-                // $this->pdo = new PDO("sqlsrv:server = tcp:transbus-server.database.windows.net; Database = dbtg", "admindbce", "C4n3l0BD!!.");
-                ////$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$this->pdo = new PDO("sqlsrv:server = $hostname; Database= $dbname", "$username", "$password");
+                // $hostname = 'tcp:transbus-server.database.windows.net,1433';
+                // $dbname = 'tbg';
+                // $username = 'transbus-server-admin';
+                // $password = "Tr4nsb4s!!.";
+                require("varl.php");
 				//$this->pdo = new PDO("sqlsrv:server = tcp:transbus-server.database.windows.net,1433; Database = tbg", "transbus-server-admin", "Tr4nsb4s!!.");
+				$this->pdo = new PDO("sqlsrv:server = $hostname; Database= $dbname", "$username", "$password");
     			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             }
             catch(PDOException $e)
--- varl.php
+++ varl.php
@@ -1,13 +1,7 @@
 <?php
-    $hostname = "tcp:transbus-server.database.windows.net,1433";
-    $dbname = "tbg";
-    $username = "transbus-server-admin";
+    $hostname = 'tcp:transbus-server.database.windows.net,1433';
+    $dbname = 'tbg';
+    $username = 'transbus-server-admin';
     $password = "Tr4nsb4s!!.";
-
-
-    // define("hostname","srv-dbtransbus.database.windows.net");
-    // define("dbname","tbg");
-    // define("username","admindbce");
-    // define("password","C4n3l0BD!!.");
 
 ?>
